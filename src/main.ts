92400e9c688d6fd0efbd75f3738510157ac0bcd5
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2024-11-06 22:31:26 +0700

    CPLERINGS-149: Enable large request body

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { TransformInterceptor } from './util/transform.interceptor';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   app.enableCors({
     origin: [
